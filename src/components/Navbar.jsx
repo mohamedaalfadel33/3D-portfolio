acb358412abaaffeee2e362d4ce2ec87e7a86af8
MOHAMED <mohamed1239455@gmail.com>
2023-10-25 09:09:33 -0800

    fix screen mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,22 +11,22 @@ const Navbar = () => {
     <nav
       className={`${styles.paddingX}  w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className=" w-full flex justify-between  items-center max-w-7xl mx-auto">
+      <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
         <Link
           to="/"
-          className=" flex items-center gap-2"
+          className="flex items-center gap-2 "
           onClick={() => {
             setActive('');
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className=" w-9 h-9 object-contain" />
+          <img src={logo} alt="logo" className="object-contain w-9 h-9" />
           <p className=" text-white text-[18px] font-bold cursor-pointer flex">
             Mohamed&nbsp;
-            <span className=" sm:block hidden">| Dzn</span>
+            <span className="hidden sm:block">| Dzn</span>
           </p>
         </Link>
-        <ul className=" list-none hidden sm:flex flex-row gap-10">
+        <ul className="flex-row hidden gap-10 list-none sm:flex">
           {navLinks.map((link) => (
             <li
               key={link.id}
@@ -40,7 +40,7 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div className="flex items-center justify-end flex-1 sm:hidden">
           <img
             src={toggle ? close : menu}
             alt="menu"
@@ -50,9 +50,9 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? 'hidden' : 'flex'
-            } p-6 violet-gradient absolute top-16  -right-4 mx-4 my-2 min-[140px] z-10 rounded-xl`}
+            } p-6 violet-gradient absolute top-16  -right-4 mx-4 my-2 min-[140px] z-10 rounded-xl w-full   justify-center`}
           >
-            <ul className=" list-none flex justify-end items-start flex-col gap-4">
+            <ul className="flex flex-col items-start justify-end gap-4 list-none ">
               {navLinks.map((link) => (
                 <li
                   key={link.id}
